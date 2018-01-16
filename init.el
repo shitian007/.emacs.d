e86f8a65847fa96d5f58ec752d344875b31389a5
DF <dfwong5566@gmail.com>
2018-01-16 16:05:21 +0800

    lots of stuff

--- init.el
+++ init.el
@@ -1,11 +1,23 @@
-(setq gc-cons-threshold 400000000)
-;;; Set up package
-(require 'package)
-(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
-(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
-;;(when (boundp 'package-pinned-packages)
-;;  (setq package-pinned-packages
-;;        '((org-plus-contrib . "org"))))
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
 (package-initialize)
-;;; Load the config
+
+(require 'org)
 (org-babel-load-file (concat user-emacs-directory "config.org"))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (macrostep org-bullets org-bullet auctex wrap-region f yaml-mode with-editor which-key wgrep use-package rg powerline org-preview-html org-plus-contrib org-gcal neotree moe-theme linum-relative hydra htmlize flx-isearch exec-path-from-shell evil eshell-git-prompt deft dashboard counsel))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
